e51d21cdff1ce820d1c997471ba053369ae17fb6
rchen10 <rchen10@stuy.edu>
2017-12-10 20:44:30 -0500

    update client, almost done without shm

--- client.c
+++ client.c
@@ -3,6 +3,8 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
 #define SEM_KEY 22446
@@ -16,6 +18,7 @@ struct sembuf{
 int main(){
   int sem_id;
   sem_id = semget(SEM_KEY, 0, 0);
+  int fd = open("./file", 0644, O_APPEND | O_CREAT | O_WRONLY);
 
   struct sembuf sem_data;
   sem_data.sem_op = -1;
@@ -23,7 +26,14 @@ int main(){
   sem_data.sem_flags = SEM_UNDO;
 
   semop(sem_id, &sem_data, 1);
+
+  printf("Please write your next line:\n");
   char *input = (char *) calloc(500, sizeof(char));
   fgets(input, 500, stdin);
+
+  write(fd, input, 500);
+
+  sem_data.sem_op = 1;
+  semop(sem_id, &sem_data, 1);
   
 }
