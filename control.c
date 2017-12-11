b29d89a01ec44fcfab6137b3bb4cc98462696069
Pacy Yan <pyan@stuy.edu>
2017-12-10 21:45:38 -0500

    control updated

--- control.c
+++ control.c
@@ -5,38 +5,75 @@
 #include <sys/sem.h>
 #include <unistd.h>
 #include <string.h>
-#include "control.h"
-
+#include "control.h" 
 int main(int argc, char *argv[]){
-  int val = 0;
-  if (argc > 2){
-    //printf("%d\n", argv[2]);
-    if(! strcmp(argv[1], "-c")){	
-      sscanf(argv[2], "%d", &val);
-      //printf("%d\n", val);
+  int sem_id = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | 0777);
+  int shm_id = shmget(SHM_KEY, sizeof(int), IPC_CREAT | IPC_EXCL | 0777);
+  int story_file = open("story.txt", O_CREAT | O_EXCL | O_TRUNC | O_RDWR, 0777)
+  if (argc == 1){
+    printf("flags missing\n");
+  }
+  else if (! strcmp(argv[1], "-c")){
+    if (sem_id == -1 || shm_id == -1 || story_file == -1){
+      printf("error in sem, shm, and/or file\n");
+    }
+    close(story_file);
+    else{
+      //printf("semaphore created: %d\n", sem_id);
+      semctl(sem_id, 0, SETVAL, val);
+      //printf("value set: %d\n", semctl(sem_id, 0, GETVAL)) ;
+      int *shmp;
+      shm_p (int*)shmat(shm_id, 0, 0);
+      if(shm_p == -1){
+	printf("shm p error\n");
+      }
+      *shm_p = 0;
+      int shm_d = shmdt(shm_p);
+      if(d == -1){
+	printf("shmdt error\n");
+      }
+      printf("story done");
     }
   }
-  //printf("%s\n", argv[1]);
-  if (! strcmp(argv[1], "-c")){
-    int id = semget(SEM_KEY, 0, 0644 | IPC_CREAT | IPC_EXCL);
-    if (id == -1){
-      printf("semaphore already exists\n");
+  else if (! strcmp(argv[1], "-r")){
+    sem_id = semget(SEM_KEY, 1, 0777);
+    shm_id = shmget(SHM_KEY, sizeof(int), 0777);
+    if(sem_id == -1 || shm_id == -1){
+      printf("error\n");
     }
     else{
-      printf("semaphore created: %d\n", id);
-      semctl(id, 0, SETVAL, val);
-      printf("value set: %d\n", semctl(id, 0, GETVAL)) ;
+      struct sembuf buffer;
+      buffer.semf_op = -1;
+      buffer.sum_num = 0;
+      buffer.sem_flg = SEM_UNDO;
+      semop(sem_id, &buffer, 1);
+      printf("story:\n");
+      int f = fork();
+      if(f){
+	int s;
+	wait(&s);
+      }
+      else{
+	char *story_file[] = {" ", "story.txt", 0};
+      execvp(story_file[0], story_file);
+      }
+      semctl(sem_id, 0, IPC_RMID, arg);
+      shmctl(shm_id, IPC_RMID, NULL);
+      remove("story.txt");
+      printf("story removed: %d\n", semctl(semget(SEM_KEY, 0, 0), 0, IPC_RMID));
     }
   }
-
-  if (! strcmp(argv[1], "-r")){
-    printf("semaphore removed: %d\n", semctl(semget(SEM_KEY, 0, 0), 0, IPC_RMID));
-  }
-	
-  if (! strcmp(argv[1], "-v")){
-    int v = semctl(semget(SEM_KEY, 0, 0), 0, GETVAL);
-    printf("semaphore value: %d\n", v);
-    //printf("%s\n", strerror(errno));
-		
+  else if (! strcmp(argv[1], "-v")){
+    printf("story:\n");
+    int f = fork();
+    if(f){
+      int s;
+      wait(&s);
+    }
+    else{
+      char *story_file[] = {" ", "story.txt", 0};
+      execvp(story_file[0], story_file);
+    }
   }
 }
+
